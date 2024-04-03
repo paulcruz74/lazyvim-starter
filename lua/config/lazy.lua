aedde16f8aa2707853c480d7f46a3f3b7cf797fd
Paul Cruz <paulcruz74@gmail.com>
2024-04-02 19:12:25 -0500

    Update colorscheme to catppuccin and add language servers

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.clangd" },
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
